ea0b732b5cb26bb7df75cef97fe6d6c244469ab8
alejo martinez <alejoomartinex11@gmail.com>
2023-08-24 21:01:02 -0300

    rutas corregidas

--- src/public/js/charlas.js
+++ src/public/js/charlas.js
@@ -3,7 +3,7 @@ const texto = document.getElementById('message');
 
 const enviarMsg = async()=>{
     const data = texto.value;
-    const response = await fetch('http://localhost:8007/api/user/', {
+    const response = await fetch('https://blog-luz-en-el-camino-production.up.railway.app/api/user/', {
         method:'POST',
         body:JSON.stringify({text: data}),
         headers:{
